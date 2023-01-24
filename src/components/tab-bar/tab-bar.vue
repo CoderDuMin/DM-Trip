558532595df882cd13e9ddefdc9cfba07ae0a6a4
CoderDuMin <1325964835@qq.com>
2023-01-24 16:32:12 +0800

    标签栏封装、引入vant

--- src/components/tab-bar/tab-bar.vue
+++ src/components/tab-bar/tab-bar.vue
@@ -1,53 +1,34 @@
 <template>
   <div class="tab-bar">
-    <template v-for="(item,index) in tabbarData" :key="item.path">
+    <!-- <template v-for="(item,index) in tabbarData" :key="item.path">
       <div :class="{'tab-item':true,'active':$route.path == item.path}" @click="tabClick(item)">
         <img :src="getImgUrl($route.path == item.path ? item.activeImg : item.img)" alt="">
         <span class="title">{{ item.text }}</span>
       </div>
-    </template>
+    </template> -->
+    <van-tabbar route v-model="currentIndex" active-color="#ff9854">
+      <van-tabbar-item v-for="(item,index) in tabbarData" icon="home-o" :to="item.path">
+        <span>{{ item.text }}</span>
+        <template #icon>
+          <img :src="getImgUrl(currentIndex == index ? item.activeImg : item.img)" alt="" />
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
 import { tabbarData } from '@/assets/data/tab-bar'
 import { getImgUrl } from '@/utils/utils'
-
-const router = useRouter()
-
-const tabClick = (item) => {
-  console.log('点击tab',item)
-  router.push({
-    path:item.path
-  })
-}
+const currentIndex = ref(0)
 
 </script>
 
 <style scoped lang="less">
 .tab-bar{
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 50px;
-  display: flex;
-  border-top: 1px solid #f3f3f3;
-  .tab-item{
-    flex:1;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    &.active{
-      color:orange;
-    }
-    img{
-      width: 36px;
-      height: 36px;
-    }
+  img{
+    height: 24px;
   }
 }
 </style>
